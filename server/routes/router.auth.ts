3329631120973d40a99a98bfda530d50b824246c
MateoTaito <mtaito@alumnos.uai.cl>
2024-06-11 18:36:03 -0400

    comiteate ctm

--- server/routes/router.auth.ts
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { AuthController } from "../controllers/AuthControllers";
-import { body } from "express-validator";
-import { handleInputErros } from "../middleware";
-import { param } from "express-validator";
---
+++ server/src/controllers/AuthControllers.ts
@@ -0,0 +1,285 @@
+import type { Request, Response } from "express";
+import User from "../models/Paciente.model";
+import Consulta from "../models/Consulta.model";
+import Paciente from "../models/Paciente.model";
+
+
+export class AuthController {
+    static createAccount = async (req: Request, res: Response) => {
+		try {
+			console.log("Creando paciente con", req.body);
+
+            const user_exists = await User.findOne({ where: { Rut: req.body.rut } });
+			console.log("Resultado Busqueda", user_exists);
+
+            if (!user_exists) {
+				console.log("Creando nuevo paciente");
+				const user = new User();
+				user.Telefono = req.body.numero;
+				user.Nombre = req.body.nombre;
+                user.Rut = req.body.rut;
+                user.Dirección = req.body.direccion;
+				await Promise.allSettled([user.save()]);
+			}
+
+            const user = await User.findOne({ where: { Rut: req.body.rut } });
+			console.log("Resultado de Persona", user.Nombre, user.Rut, user.Telefono, user.Dirección);
+
+			if (user_exists) {
+				const error = new Error("Este rut ya se encuentra registrado");
+				return res.status(409).json({ error: error.message });
+			}
+			res.send("Operacion exitosa");
+        }
+        catch (error) {
+			console.log(error);
+			res.status(500).json({ error: "Hubo un error" });
+		}
+    };
+
+
+    static createConsulta = async (req: Request, res: Response) => {
+		try {
+			console.log("Creando consulta con", req.body);
+
+            const consulta_exists = await Consulta.findOne({ where: { Id: req.body.id } });
+			console.log("Resultado Busqueda", consulta_exists);
+
+            if (!consulta_exists) {
+				console.log("Creando nueva consulta");
+				const consulta = new Consulta();
+				consulta.Id = req.body.id;
+				consulta.fechaConsulta = req.body.fechaConsulta;
+                consulta.horaConsulta = req.body.horaConsulta;
+                consulta.medico_tratante = req.body.medico_tratante;
+                consulta.nro_clinica = req.body.nro_clinica;
+                consulta.Paciente_Rut = req.body.paciente_rut;
+				await Promise.allSettled([consulta.save()]);
+			}
+
+            const consulta = await Consulta.findOne({ where: { Id: req.body.id } });
+			console.log("Resultado de Consulta", consulta.Id, consulta.fechaConsulta, consulta.horaConsulta, consulta.medico_tratante);
+
+
+			if (consulta_exists) {
+				const error = new Error("Este Id ya se encuentra registrado");
+				return res.status(409).json({ error: error.message });
+			}
+			res.send("Operacion exitosa");
+
+        }
+        catch (error) {
+			console.log(error);
+			res.status(500).json({ error: "Hubo un error" });
+		}
+	};
+
+
+		static editarPaciente = async (req: Request, res: Response) => {
+			try {
+				console.log("Paciente a editar", req.body.rut);
+	
+				const user_exists = await User.findOne({ where: { Rut: req.body.rut } });
+				console.log("Resultado Busqueda", user_exists);
+	
+				if (user_exists) {
+					console.log("Editando nuevo paciente");
+					const user = await User.findOne(req.body.rut);
+					user.Telefono = req.body.numero;
+					user.Nombre = req.body.nombre;
+					user.Rut = req.body.rut;
+					user.Dirección = req.body.direccion;
+					await user.save().then(() => {
+						console.log("Usuario editado correctamente");
+						res.status(201).json({ message: "Usuario editado con éxito" });})
+				}
+	
+				const user = await User.findOne({ where: { Rut: req.body.rut } });
+				console.log("Resultado de Persona", user.Nombre, user.Rut, user.Telefono, user.Dirección);
+	
+				if (!user_exists) {
+					const error = new Error("Usuario no encontrado");
+					return res.status(409).json({ error: error.message });
+				}
+				res.send("Operacion exitosa");
+
+			}
+			catch (error) {
+				console.log(error);
+				res.status(500).json({ error: "Hubo un error" });
+			}
+    };
+
+	static editarConsulta = async (req: Request, res: Response) => {
+		try {
+			console.log("Consulta a editar", req.body.rut);
+
+			const user_exists = await Consulta.findOne({ where: { Id: req.body.id } });
+			console.log("Resultado Busqueda", user_exists);
+
+			if (user_exists) {
+				console.log("Editand consulta");
+				const consulta = await Consulta.findOne(req.body.id);
+				consulta.Id = req.body.id;
+				consulta.fechaConsulta = req.body.fechaConsulta;
+                consulta.horaConsulta = req.body.horaConsulta;
+                consulta.medico_tratante = req.body.medico_tratante;
+                consulta.nro_clinica = req.body.nro_clinica;
+                consulta.Paciente_Rut = req.body.paciente_rut;
+				await Promise.allSettled([consulta.save()]);
+			}
+
+			const consulta = await Consulta.findOne({ where: { Id: req.body.id } });
+			console.log("Resultado de Consulta", consulta.Id, consulta.fechaConsulta, consulta.horaConsulta, consulta.medico_tratante);
+
+			if (!user_exists) {
+				const error = new Error("Consulta no encontrado");
+				return res.status(409).json({ error: error.message });
+			}
+			res.send("Operacion exitosa");
+
+		}
+		catch (error) {
+			console.log(error);
+			res.status(500).json({ error: "Hubo un error" });
+		}
+	};
+
+	static deleteAccount = async (req: Request, res: Response) => {
+		try {
+			console.log("paciente a eliminar", req.body.rut);
+
+			const user_exists = await User.findOne({ where: { Rut: req.body.rut } });
+			const consulta_exists = await Consulta.findOne({ where: { Paciente_Rut: req.body.rut } });
+			console.log("Resultado Busqueda", user_exists);
+			
+
+			if (user_exists) {
+				console.log("Eliminando paciente");
+				const user = await User.findOne(req.body.rut);
+				const consulta = await Consulta.findOne(req.body.rut );
+				if (consulta_exists) {await consulta.destroy();}
+				await user.destroy();
+			}
+
+			if (!user_exists) {
+				const error = new Error("paciente no encontrado");
+				return res.status(409).json({ error: error.message });
+			}
+			res.send("Operacion exitosa");
+
+		}
+		catch (error) {
+			console.log(error);
+			res.status(500).json({ error: "Hubo un error" });
+		}
+	};
+
+	static deleteConsulta = async (req: Request, res: Response) => {
+		try {
+			console.log("Consulta a eliminar", req.body.id);
+
+			const consulta_exists = await Consulta.findOne({ where: { Id: req.body.id } });
+			console.log("Resultado Busqueda", consulta_exists);
+			
+
+			if (consulta_exists) {
+				console.log("Eliminando consultas");
+				const consulta = await Consulta.findOne(req.body.id);
+				await consulta.destroy();
+			}
+
+			if (!consulta_exists) {
+				const error = new Error("Consulta no encontrada");
+				return res.status(409).json({ error: error.message });
+			}
+			res.send("Operacion exitosa");
+
+		}
+		catch (error) {
+			console.log(error);
+			res.status(500).json({ error: "Hubo un error" });
+		}
+	};
+
+	static getConsulta = async (req: Request, res: Response) => {
+		try {
+			console.log("Consulta a buscar", req.body.id);
+
+			const consulta_exists = await Consulta.findOne({ where: { Id: req.body.id } });
+			console.log("Resultado Busqueda", consulta_exists.dataValues);
+
+			if (!consulta_exists) {
+				const error = new Error("Consulta no encontrada");
+				return res.status(409).json({ error: error.message });
+			}
+			res.send("Operacion exitosa");
+
+		}
+		catch (error) {
+			console.log(error);
+			res.status(500).json({ error: "Hubo un error" });
+		}
+	};
+
+	static getPaciente = async (req: Request, res: Response) => {
+		try {
+			console.log("Paciente a consultar", req.body.id);
+
+			const Paciente_exists = await Paciente.findOne({ where: { Rut: req.body.rut} });
+			console.log("Resultado Busqueda", Paciente_exists.dataValues);
+			
+
+			if (!Paciente_exists) {
+				const error = new Error("Consulta no encontrada");
+				return res.status(409).json({ error: error.message });
+			}
+			res.send("Operacion exitosa");
+
+		}
+		catch (error) {
+			console.log(error);
+			res.status(500).json({ error: "Hubo un error" });
+		}
+	};
+
+	static getPacienteList = async (req: Request, res: Response) => {
+		try {
+
+			const Paciente_exists = await Paciente.findAll();
+			console.log("Resultado Busqueda", Paciente_exists);
+			
+
+			if (!Paciente_exists) {
+				const error = new Error("No existen pacientes");
+				return res.status(409).json({ error: error.message });
+			}
+			res.send("Operacion exitosa");
+
+		}
+		catch (error) {
+			console.log(error);
+			res.status(500).json({ error: "Hubo un error" });
+		}
+	};
+
+	static getConsultaList = async (req: Request, res: Response) => {
+		try {
+
+			const Consulta_exists = await Consulta.findAll();
+			console.log("Resultado Busqueda", Consulta_exists);
+			
+
+			if (!Consulta_exists) {
+				const error = new Error("No existen pacientes");
+				return res.status(409).json({ error: error.message });
+			}
+			res.send("Operacion exitosa");
+
+		}
+		catch (error) {
+			console.log(error);
+			res.status(500).json({ error: "Hubo un error" });
+		}
+	};
+}
